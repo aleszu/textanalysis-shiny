7b966e5ab61cfac36e670e6403f4a50f45991011
aleszu <aleszu.bajak@gmail.com>
2018-10-12 07:52:11 -0400

    Add files via upload

--- csvanalysis/app.R
+++ csvanalysis/app.R
@@ -11,7 +11,7 @@ ui <- fluidPage(
   titlePanel("Drag-and-drop CSV textual analysis"),
   tags$div(class="header", checked=NA,
            tags$p("Upload a CSV file with 'date' and 'headline' columns to run an exploratory sentiment analysis. 
-                  Your 'date' column should have a MM/DD/YYYY or MM-DD-YYYY format.")),
+                  Your 'date' column should have a MM/DD/YYYY or MM-DD-YYYY format. Files should not exceed 5MB.")),
   hr(),
   
   sidebarLayout(
@@ -19,24 +19,23 @@ ui <- fluidPage(
     
     sidebarPanel(
       fileInput("file", "Upload your CSV file"),
-      selectInput("smooth", "Choose number of words to score", choices = c("25","50","100", "150")),
+      selectInput("smooth", "Choose number of words to score", choices = c("50","100", "150")),
       tags$div(class="header", checked=NA,
                tags$p("By choosing 50, for example, you'll only be visualizing the 50 most negative and 50 most positive words across time.")),
       hr(),
-      textAreaInput("keyword", "Optional search for specific names"),
+      textAreaInput("keyword", "Search for specific words over time"),
       tags$div(class="header", checked=NA,
-               tags$p("Try looking for specific keywords to see their use over time.")),
+               tags$i("Optional")),
       hr(),
-      #     actionButton("update", "Update"),
-      #     hr()
       tags$div(class="header", checked=NA,
                tags$p("Some datasets you might be curious to explore."),
                tags$a(href="https://raw.githubusercontent.com/aleszu/textanalysis-shiny/master/r-politics-three-months.csv", "Three months of r/politics headlines"),
                tags$br(),
-               tags$a(href="https://raw.githubusercontent.com/aleszu/textanalysis-shiny/master/trumptweets-nov16.csv", "@realDonaldTrump tweets through Nov. 2016"),
-               tags$br(),
-               tags$a(href="https://raw.githubusercontent.com/aleszu/textanalysis-shiny/master/murkowski.csv", "Six months of Sen. Lisa Murkowski articles")),
+               tags$a(href="https://raw.githubusercontent.com/aleszu/textanalysis-shiny/master/Heitkamp-articles-3-months.csv", "Three months of Sen. Heidi Heitkamp articles"),
+               tags$br(),         
+               tags$a(href="https://raw.githubusercontent.com/aleszu/textanalysis-shiny/master/trumptweets-nov16.csv", "@realDonaldTrump tweets through Nov. 2016")),
       tags$br(),
+      hr(),
       tags$div(class="header", checked=NA,
                tags$p("Have a TXT file for sentiment analysis?"),
                tags$a(href="https://storybench.shinyapps.io/textanalysis/", "Try out my other drag-and-drop app.")),
@@ -46,23 +45,31 @@ ui <- fluidPage(
       ),
     
     mainPanel(
-    #  hr(),
-    #  h4("Sentiment of top 25 most negative and positive words across time", align = "center"),
-    #  plotOutput("p_sentC"),
-    #  hr(),
-      h4("Sentiment analysis of headlines with smoothing", align = "center"),
-      plotOutput("p_smooth"),
-      hr(),
-      h4("Headlines - or keyword - over time", align = "center"),
-      plotOutput("keywordplot"),
-      h4("Scores and context of the most positive words", align = "center"),
-      DTOutput("tbtop50"),
-      hr(),
-      h4("Scores and context of the most negative words", align = "center"),
-      DTOutput("tbbot50"),
-      hr(),
-      h4("Full dataset", align = "center"),
-      DTOutput('tb')
+      # tabsetPanel(type = "tabs",
+      #             tabPanel("Sentiment", plotOutput("p_smooth")),
+      #             tabPanel("Time series", plotOutput("keywordplot")),
+      #             tabPanel("Top terms", plotOutput("topterms")),
+      #             tabPanel("Positive terms", DTOutput("tbtop50")),
+      #             tabPanel("Negative terms", DTOutput("tbbot50")),
+      #             tabPanel("Full dataset", DTOutput("tb"))
+      # )
+    
+      
+          h4("Sentiment analysis of headlines with smoothing", align = "center"),
+          plotOutput("p_smooth"),
+          hr(),
+          h4("Headlines - or keyword - over time", align = "center"),
+          plotOutput("keywordplot"),
+          h4("Top terms", align = "center"),
+          plotOutput("topterms"),
+          h4("Scores and context of the most positive words", align = "center"),
+          DTOutput("tbtop50"),
+          hr(),
+          h4("Scores and context of the most negative words", align = "center"),
+          DTOutput("tbbot50"),
+          hr(),
+          h4("Full dataset", align = "center"),
+          DTOutput('tb')
     )
   )
 )
@@ -80,14 +87,51 @@ server <- function(input, output, session) {
     
   })
   
-  # output$column <- renderUI({
-  #   df <- filedata()
-  #   if (is.null(df)) return(NULL)
-  #   nums <- sapply(df, is.numeric) # keep only number columns
-  #   items=names(nums[nums]) # keep only number columns
-  #   names(items)=items
-  #   selectInput("date", "Choose column with date either in MM/DD/YYYY or MM-DD-YYYY format", items)
-  # })
+  output$p_smooth <- renderPlot({
+    
+    if (is.null(input$file)){
+      return(NULL)      
+    }
+    
+    library(dplyr)
+    library(tidyverse)
+    library(tidytext)
+    library(ggplot2)
+    
+    df <- filedata()
+    df$date <- as.Date(df$date, format = "%m/%d/%Y")
+    df$headlines <- df$headline
+    
+    sentiments <- read.csv("https://raw.githubusercontent.com/aleszu/textanalysis-shiny/master/labMT2english.csv", sep="\t")
+    labMT <- sentiments %>%
+      select(word, happs)
+    
+    tokenizedC <- df %>%
+      select(date, headline, headlines) %>% #input date column 
+      unnest_tokens(word, headline) %>%
+      anti_join(stop_words) %>%
+      inner_join(labMT, by = "word") %>%
+      group_by(word, date, headlines) %>%
+      summarize(sentiment = mean(happs)) %>%
+      arrange(desc(sentiment)) %>%
+      mutate("score" = sentiment-5.372 ) 
+    
+    topsentC <- head(arrange(tokenizedC,desc(score)), n=input$smooth) 
+    
+    bottomsentC <- head(arrange(tokenizedC,score), n=input$smooth) 
+    
+    sentimentC <- rbind(topsentC, bottomsentC)
+    
+    p_smooth <- ggplot(sentimentC, aes(x = date, y = score)) +
+      geom_smooth() +
+      ylab("sentiment") +
+      xlab("time") +
+      geom_hline(yintercept=0, linetype="dashed") +
+      scale_x_date(date_labels = "%b %d")
+    p_smooth
+    
+    
+  })
 
   output$p_sentC <- renderPlot({
     if (is.null(input$file)){
@@ -140,50 +184,7 @@ server <- function(input, output, session) {
     
   })
   
-  output$p_smooth <- renderPlot({
-    
-    if (is.null(input$file)){
-      return(NULL)      
-    }
-    
-    library(dplyr)
-    library(tidyverse)
-    library(tidytext)
-    library(ggplot2)
-    
-    df <- filedata()
-    df$date <- as.Date(df$date, format = "%m/%d/%Y")
-    df$headlines <- df$headline
-    
-    sentiments <- read.csv("https://raw.githubusercontent.com/aleszu/textanalysis-shiny/master/labMT2english.csv", sep="\t")
-    labMT <- sentiments %>%
-      select(word, happs)
-    
-    tokenizedC <- df %>%
-      select(date, headline, headlines) %>% #input date column 
-      unnest_tokens(word, headline) %>%
-      anti_join(stop_words) %>%
-      inner_join(labMT, by = "word") %>%
-      group_by(word, date, headlines) %>%
-      summarize(sentiment = mean(happs)) %>%
-      arrange(desc(sentiment)) %>%
-      mutate("score" = sentiment-5.372 ) 
-
-    topsentC <- head(arrange(tokenizedC,desc(score)), n=input$smooth) 
-
-    bottomsentC <- head(arrange(tokenizedC,score), n=input$smooth) 
-
-    sentimentC <- rbind(topsentC, bottomsentC)
-
-    p_smooth <- ggplot(sentimentC, aes(x = date, y = score)) +
-      geom_smooth() +
-      ylab("sentiment") +
-      xlab("time") +
-      scale_x_date(date_breaks = "1 month")
-    p_smooth
-    
-    
-  })
+ 
   
     # Searching keyword
   
@@ -218,7 +219,42 @@ server <- function(input, output, session) {
     })
     
     
-  
+   # top terms 
+    
+    output$topterms <- renderPlot({ 
+      
+      if (is.null(input$file)){
+        return(NULL)      
+      }
+      
+      library(dplyr)
+      library(tidyverse)
+      library(tidytext)
+      library(ggplot2)
+      
+      df <- filedata()
+      df$date <- as.Date(df$date, format = "%m/%d/%Y")
+      df$headlines <- df$headline
+      
+      tokenizedC <- df %>%
+        select(date, headline, headlines) %>% 
+        unnest_tokens(word, headline) %>%
+        anti_join(stop_words) %>%
+        group_by(word) %>%
+        tally()
+      
+      topterms <- tokenizedC %>%
+        top_n(20) %>%
+        arrange(desc(n)) %>%
+        ggplot(aes(x = reorder(word, -n), y = n)) +
+        geom_col() +
+        labs(x = NULL, y = "frequency") +
+        coord_flip() +
+        theme(legend.position = "none")
+      topterms
+      
+    })
+    
   
     # Table 1
     
